6d12b230a9917506c5ba31e3394e0435b5c8967e
Tomas Kantor <kantor.t99@gmail.com>
2023-03-06 22:41:11 +0100

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -127,6 +127,7 @@ bool handle_arguments(int argc, char *argv[], Arguments& arguments)
     return true;
 }
 
+// converts uppercase or lowercase letter to position in alphabet
 int char_to_num(char c)
 {
     if ( c >= 'A' and c <= 'Z')
@@ -140,6 +141,7 @@ int char_to_num(char c)
     return -1;
 }
 
+// encrypts input string given a and b
 std::string encrypt(Arguments& arguments)
 {
     int a = arguments.a;
@@ -160,9 +162,10 @@ std::string encrypt(Arguments& arguments)
     return answer;
 }
 
+// finds multiplicative inverse by looping over candidates
 int multiplicative_inverse(int a)
 {
-    for (int i = 0; i < alphabet_size; i++)
+    for (int i = 1; i < alphabet_size; i += 2)
     {
         if ( ( i*a ) % alphabet_size == 1 )
         {
@@ -172,6 +175,7 @@ int multiplicative_inverse(int a)
     return -1;
 }
 
+// decrypts input string given a and b
 std::string decrypt(Arguments& arguments)
 {
     int a_inverse = multiplicative_inverse(arguments.a);
@@ -197,6 +201,7 @@ std::string decrypt(Arguments& arguments)
     return answer;
 }
 
+// return vector of counts of characters in str
 std::vector<int> get_counts(std::string str)
 {
     std::vector<int> counts(alphabet_size, 0);
@@ -211,6 +216,7 @@ std::vector<int> get_counts(std::string str)
     return counts;
 }
 
+// computes deviation between two vectors
 float get_deviation(const std::vector<float> v1, const float v2[])
 {
     float deviation = 0.0;
@@ -221,6 +227,7 @@ float get_deviation(const std::vector<float> v1, const float v2[])
     return deviation;
 }
 
+// trys all keys, computes deviations and saves best pair to arguments.a and arguments.b
 float brute_force_analysis(Arguments& arguments)
 {
     std::vector<int> counts = get_counts(arguments.input_string);
@@ -229,16 +236,20 @@ float brute_force_analysis(Arguments& arguments)
     int best_a_inverse;
     int best_b;
     float best_deviation = alphabet_size;
+    // loop over a inverse get a later
     for ( int a_inverse : {1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25 })
     {
         for ( int b = 0; b < alphabet_size; b++ )
         {
+            // rearrange counts based on key par (a, b)
             for ( int x = 0; x < alphabet_size; x++)
             {
                 int index_x = ( a_inverse*( (x - b + alphabet_size)) )%alphabet_size;
                 count_guess[index_x] = float(counts[x]) / sum_counts;
             }
+            // get deviation
             float deviation = get_deviation(count_guess, frequencies);
+            // find best deviation
             if ( deviation < best_deviation)
             {
                 best_a_inverse = a_inverse;
@@ -247,6 +258,7 @@ float brute_force_analysis(Arguments& arguments)
             }            
         }
     }
+    // find a based on a inverse
     int best_a = multiplicative_inverse(best_a_inverse);
     arguments.a = best_a;
     arguments.b = best_b;
@@ -285,11 +297,15 @@ void test_csv()
 
 void decrypt_unknown_key(Arguments& arguments)
 {
+    // get input string
     std::fstream input_stream;
     input_stream.open(arguments.input_file, ios::in);
     getline(input_stream, arguments.input_string);
+    // find keys
     brute_force_analysis(arguments);
+    // decrypt text
     std::string answer = decrypt(arguments);
+    // output decrypt text to file
     std::ofstream output_stream;
     output_stream.open(arguments.output_file);
     output_stream << answer;
